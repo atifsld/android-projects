98beb40b7d50c1db3e4797e48da795f6716db656
Atif Saleed <atif.saleed@publicissapient.com>
2022-07-22 13:48:04 +0530

    First activity made

--- HelloJohn2/app/src/main/java/com/atif/hellojohn/MainActivity.kt
+++ HelloJohn2/app/src/main/java/com/atif/hellojohn/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.atif.hellojohn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val submitButton = findViewById<Button>(R.id.submit_button)
+        val personNameEditText = findViewById<TextView>(R.id.personName)
+        val singupButton = findViewById<Button>(R.id.singup_button)
+        submitButton.setOnClickListener {
+            val personName = personNameEditText.text
+            val helloTextView = findViewById<TextView>(R.id.helloText)
+            if(personName == "") {
+                singupButton.visibility = INVISIBLE
+                Toast.makeText(
+                    this,
+                    "Please enter your name!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                singupButton.visibility = VISIBLE
+                val newHelloMessage = "Hello $personName"
+                helloTextView.text = newHelloMessage
+            }
+        }
+
+        singupButton.setOnClickListener {
+            val personName = personNameEditText.text
+            Intent intent = new Intent(this, )
+        }
     }
 }
