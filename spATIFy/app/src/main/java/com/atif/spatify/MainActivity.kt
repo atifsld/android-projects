6625de711ac946d46ba276a5e74ab605b6367ad1
Atif Saleed <atif.mohd.sld@gmail.com>
2022-07-25 23:57:10 +0530

    Skeletons for albums and songs fragments

--- spATIFy/app/src/main/java/com/atif/spatify/MainActivity.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/MainActivity.kt
@@ -2,10 +2,30 @@ package com.atif.spatify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val albumButton = findViewById<Button>(R.id.albumButton)
+        val songButton = findViewById<Button>(R.id.songButton)
+
+        albumButton.setOnClickListener {
+            val albumsFragment = AlbumsFragment()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.contentLayout, albumsFragment)
+            transaction.commit()
+            println("albumButton clicked.")
+        }
+
+        songButton.setOnClickListener {
+            val songsFragment = SongsFragment()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.contentLayout, songsFragment)
+            transaction.commit()
+            println("songButton clicked.")
+        }
     }
 }
