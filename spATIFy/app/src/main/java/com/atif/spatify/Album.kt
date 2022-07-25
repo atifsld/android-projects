614505bfad494fc6f2af575bf50a87080b35d224
Atif Saleed <atif.mohd.sld@gmail.com>
2022-07-26 02:34:06 +0530

    Corrected data type of albumArtists

--- spATIFy/app/src/main/java/com/atif/spatify/Album.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/Album.kt
@@ -6,7 +6,7 @@ data class Album (
     @SerializedName("album_uuid") val albumUuid: String,
     @SerializedName("album_name") val albumName: String,
     @SerializedName("album_art_url") val albumArtUrl: String,
-    @SerializedName("album_artists") val albumArtist: String,
+    @SerializedName("album_artists") val albumArtists: List<String>,
     @SerializedName("album_credits") val albumCredits: List<AlbumCredit>,
     @SerializedName("album_songs") val albumSongs: List<Song>,
     @SerializedName("album_label") val albumLabel: List<String>,
@@ -17,7 +17,7 @@ data class Album (
         return "Album UUID: $albumUuid" + "\n" +
                 "Album name: $albumName" + "\n" +
                 "Album art URL: $albumArtUrl" + "\n" +
-                "Album artist: $albumArtist" + "\n" +
+                "Album artist: $albumArtists" + "\n" +
                 "Album credits: $albumCredits" + "\n" +
                 "Album songs: $albumSongs" + "\n" +
                 "Album label: $albumLabel" + "\n" +
