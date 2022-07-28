eb29e3933d44abd1aab2a9b2fa37d396c8e94260
Atif Saleed <atif.mohd.sld@gmail.com>
2022-07-28 22:36:16 +0530

    Navbar impemented

--- spATIFy/app/src/main/java/com/atif/spatify/view/activity/MainActivity.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/view/activity/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.atif.spatify.view.activity
 
+import android.content.ClipData
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.atif.spatify.R
 import com.atif.spatify.view.fragments.AlbumsFragment
 import com.atif.spatify.view.fragments.SongsFragment
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarToggle: ActionBarDrawerToggle
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +31,30 @@ class MainActivity : AppCompatActivity() {
         val albumButton = findViewById<Button>(R.id.albumButton)
         val songButton = findViewById<Button>(R.id.songButton)
 
-        loadAlbumFragment()
+        drawerLayout = findViewById(R.id.drawerLayout)
+        actionBarToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
+        drawerLayout.addDrawerListener(actionBarToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBarToggle.syncState()
+        navView = findViewById(R.id.navView)
+
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.about -> {
+                    Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.contactus -> {
+                    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:7356015305"))
+                    startActivity(intent)
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
 
         albumButton.setOnClickListener {
             val albumsFragment = AlbumsFragment()
@@ -36,6 +72,20 @@ class MainActivity : AppCompatActivity() {
             println("songButton clicked.")
         }
 
+        loadAlbumFragment()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        drawerLayout.openDrawer(navView)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     private fun loadAlbumFragment() {
