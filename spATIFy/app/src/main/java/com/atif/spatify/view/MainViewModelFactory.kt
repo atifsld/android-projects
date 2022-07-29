9a0bee109ccceaed1324381c6fe5eeda320069ef
Atif Saleed <atif.mohd.sld@gmail.com>
2022-07-29 16:00:34 +0530

    Unimplemented MVVM classes commented

--- spATIFy/app/src/main/java/com/atif/spatify/view/MainViewModelFactory.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/view/MainViewModelFactory.kt
@@ -1,13 +1,13 @@
 package com.atif.spatify.view
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.atif.spatify.service.SpatifyService
-import com.atif.spatify.view.viewmodel.MainViewModel
-
-class MainViewModelFactory(private val spatifyService: SpatifyService): ViewModelProvider.Factory{
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return MainViewModel(spatifyService) as T
-    }
-
-}
+//import androidx.lifecycle.ViewModel
+//import androidx.lifecycle.ViewModelProvider
+//import com.atif.spatify.service.SpatifyService
+//import com.atif.spatify.view.viewmodel.MainViewModel
+//
+//class MainViewModelFactory(private val spatifyService: SpatifyService): ViewModelProvider.Factory{
+//    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+//        return MainViewModel(spatifyService) as T
+//    }
+//
+//}
--- spATIFy/app/src/main/java/com/atif/spatify/view/viewmodel/MainViewModel.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/view/viewmodel/MainViewModel.kt
@@ -1,16 +1,24 @@
 package com.atif.spatify.view.viewmodel
 
-import androidx.lifecycle.ViewModel
-import com.atif.spatify.data.Album
-import com.atif.spatify.data.Song
-import com.atif.spatify.service.SpatifyService
+//import androidx.lifecycle.LiveData
+//import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.ViewModel
+//import com.atif.spatify.data.Album
+//import com.atif.spatify.data.Song
+//import com.atif.spatify.service.SpatifyService
 
-class MainViewModel(val spatifyService: SpatifyService): ViewModel() {
-    fun getAlbums(): List<Album> {
-        return spatifyService.getAlbums()
-    }
-
-    fun getSongs(): List<Song> {
-        return spatifyService.getSongs()
-    }
-}
+//class MainViewModel(private val repository: AlbumRepository): ViewModel() {
+//    private val _albums = MutableLiveData<List<Album>>()
+//    val albums: LiveData<List<Album>>
+//        get() = _albums
+//
+//
+//    suspend fun getAlbums() {
+//        val albums = repository.getAlbums()
+//        _albums.value = albums
+//    }
+//
+//    fun getSongs(): List<Song> {
+//        return spatifyService.getSongs()
+//    }
+//}
