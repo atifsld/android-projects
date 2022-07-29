7f197d298020659afe15ca326f35ffdf6c32997e
Atif Saleed <atif.mohd.sld@gmail.com>
2022-07-29 16:04:51 +0530

    Share functionality added to share button

--- spATIFy/app/src/main/java/com/atif/spatify/view/SongsInAlbumViewAdapter.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/view/SongsInAlbumViewAdapter.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
@@ -25,14 +26,14 @@ class SongsInAlbumViewAdapter(var recyclerDataArrayList: List<Song>, var context
         // Set the data to textview and imageview.
         val song: Song = recyclerDataArrayList[position]
         holder.song_title.text = song.songName
-        holder.song_title.setOnClickListener {
+        holder.song_artist.text = song.getCommaSeparatedArtists()
+        holder.song_track_number.text = song.songTrackNumber.toString()
+        holder.share_button.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND)
             intent.setType("text/plain")
             intent.putExtra(Intent.EXTRA_TEXT, song.createShareString())
             context.startActivity(intent)
         }
-        holder.song_artist.text = song.getCommaSeparatedArtists()
-        holder.song_track_number.text = song.songTrackNumber.toString()
     }
 
     override fun getItemCount(): Int {
@@ -44,5 +45,6 @@ class SongsInAlbumViewAdapter(var recyclerDataArrayList: List<Song>, var context
         val song_title: TextView = itemView.findViewById(R.id.song_title)
         val song_artist: TextView = itemView.findViewById(R.id.song_artist)
         val song_track_number: TextView = itemView.findViewById(R.id.song_track_number)
+        val share_button: ImageButton = itemView.findViewById(R.id.share_button)
     }
 }
