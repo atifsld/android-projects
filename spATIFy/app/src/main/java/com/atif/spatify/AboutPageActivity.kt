8b96e51910ff5ac0b84fb48a2d0de6bda4d07b04
Atif Saleed <atif.mohd.sld@gmail.com>
2022-07-30 19:48:05 +0530

    Created about page

---
+++ spATIFy/app/src/main/java/com/atif/spatify/AboutPageActivity.kt
@@ -0,0 +1,27 @@
+package com.atif.spatify
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import mehdi.sakout.aboutpage.AboutPage
+import mehdi.sakout.aboutpage.Element
+
+
+class AboutPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val aboutPage: View = AboutPage(this)
+            .isRTL(false)
+            //        .setImage(R.drawable.logo)
+            .addItem(Element().setTitle("Version 1.0"))
+            .addGroup("Connect with us")
+            .addEmail("atif.mohd.sld@gmail.com")
+            .addFacebook("the.medy")
+            .addTwitter("atifsld")
+            .addGitHub("atifsld")
+            .create()
+
+        setContentView(aboutPage)
+    }
+}
