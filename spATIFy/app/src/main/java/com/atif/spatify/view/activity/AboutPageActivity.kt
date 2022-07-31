c69affeb47e27506989232458ccc762c2303fac6
Atif Saleed <atif.mohd.sld@gmail.com>
2022-07-31 15:11:35 +0530

    AboutPage moved to right folder within project structure

--- spATIFy/app/src/main/java/com/atif/spatify/view/activity/AboutPageActivity.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/view/activity/AboutPageActivity.kt
@@ -1,4 +1,4 @@
-package com.atif.spatify
+package com.atif.spatify.view.activity
 
 import android.os.Bundle
 import android.view.View
--- spATIFy/app/src/main/java/com/atif/spatify/view/activity/MainActivity.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/view/activity/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.atif.spatify.view.activity
 
-import android.content.ClipData
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import com.atif.spatify.AboutPageActivity
 import com.atif.spatify.R
 import com.atif.spatify.view.fragments.AlbumsFragment
 import com.atif.spatify.view.fragments.SongsFragment
