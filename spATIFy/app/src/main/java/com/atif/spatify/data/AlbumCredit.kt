85b7602dc29270416f985f9fb50d6d78b3d0e94b
Atif Saleed <atif.mohd.sld@gmail.com>
2022-08-01 13:30:29 +0530

    Added image to album credit data object

--- spATIFy/app/src/main/java/com/atif/spatify/data/AlbumCredit.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/data/AlbumCredit.kt
@@ -2,11 +2,13 @@ package com.atif.spatify.data
 
 data class AlbumCredit (
     val creditName: String,
-    val creditContribution: String){
+    val creditContribution: String,
+    val creditImageUrl: String){
 
     override fun toString(): String {
         return "Credit name: $creditName" + "\n" +
-                "Credit contribution: $creditContribution"
+                "Credit contribution: $creditContribution" + "\n" +
+                "Credit image URL: $creditImageUrl"
     }
 
 }
