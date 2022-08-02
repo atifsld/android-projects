c1dc9520eb4af0b43d375d0937a564acca3b289d
Atif Saleed <atif.mohd.sld@gmail.com>
2022-08-02 15:53:43 +0530

    Changed SpatifyApplication class

--- spATIFy/app/src/main/java/com/atif/spatify/SpatifyApplication.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/SpatifyApplication.kt
@@ -1,49 +1,17 @@
 package com.atif.spatify
 
 import android.app.Application
-import org.json.JSONArray
-import java.io.IOException
-import java.nio.charset.Charset
+import com.atif.spatify.repository.AlbumCreditRepository
+import com.atif.spatify.repository.AlbumRepository
+import com.atif.spatify.repository.SongRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
-class SpatifyApplication: Application() {
+class SpatifyApplication : Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
 
-    var albumNameList: ArrayList<String> = ArrayList()
-    var albumArtistList: ArrayList<String> = ArrayList()
-    var albumYearList: ArrayList<Int> = ArrayList()
-
-    override fun onCreate() {
-        super.onCreate()
-        loadData()
-    }
-
-    private fun loadData() {
-        val albumArray = JSONArray(loadJSONFromAsset())
-        for(i in 0 until albumArray.length()) {
-            val albumDetail = albumArray.getJSONObject(i)
-            albumNameList.add(albumDetail.getString("album_name"))
-            albumArtistList.add(albumDetail.getString("album_artist"))
-            albumYearList.add(albumDetail.getInt("album_year"))
-        }
-        println(albumNameList)
-        println(albumArtistList)
-        println(albumYearList)
-    }
-
-    private fun loadJSONFromAsset(): String {
-        val json: String?
-        try {
-            val inputStream = assets.open("albums.json")
-            val size = inputStream.available()
-            val buffer = ByteArray(size)
-            val charset: Charset = Charsets.UTF_8
-            inputStream.read(buffer)
-            inputStream.close()
-            json = String(buffer, charset)
-        } catch (ex: IOException) {
-            println("Couldn't load data")
-            ex.printStackTrace()
-            return ""
-        }
-        return json
-    }
+    val spatifyDatabase by lazy { SpatifyRoomDatabase.getSpatifyDatabase(this, applicationScope) }
+    val albumRepository by lazy { AlbumRepository(spatifyDatabase.albumDao()) }
+    val songRepository by lazy { SongRepository(spatifyDatabase.songDao()) }
+    val albumCreditRepository by lazy { AlbumCreditRepository(spatifyDatabase.albumCreditDao()) }
 }
