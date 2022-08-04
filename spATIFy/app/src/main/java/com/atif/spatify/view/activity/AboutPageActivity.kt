aea95474500316aba0a6ca5e0e1d329efa0634b3
Atif Saleed <atif.mohd.sld@gmail.com>
2022-08-04 12:50:46 +0530

    Added description and removed Facebook page from About activity

--- spATIFy/app/src/main/java/com/atif/spatify/view/activity/AboutPageActivity.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/view/activity/AboutPageActivity.kt
@@ -12,12 +12,12 @@ class AboutPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val aboutPage: View = AboutPage(this)
+            .setDescription("Spatify was created to be a catalogue of the world's best music. Every album listed is curated by celebrated musicologists and reviewers, and is updated on a weekly basis.")
             .isRTL(false)
             //        .setImage(R.drawable.logo)
             .addItem(Element().setTitle("Version 1.0"))
             .addGroup("Connect with us")
             .addEmail("atif.mohd.sld@gmail.com")
-            .addFacebook("the.medy")
             .addTwitter("atifsld")
             .addGitHub("atifsld")
             .create()
