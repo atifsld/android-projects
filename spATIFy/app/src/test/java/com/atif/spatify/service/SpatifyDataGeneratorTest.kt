7d342dc2b06c5813e2b6cc68f55fbd1730709be9
Atif Saleed <atif.mohd.sld@gmail.com>
2022-08-06 14:28:29 +0530

    First JUnit test case

---
+++ spATIFy/app/src/test/java/com/atif/spatify/service/SpatifyDataGeneratorTest.kt
@@ -0,0 +1,31 @@
+package com.atif.spatify.service
+
+import com.atif.spatify.data.Album
+import com.atif.spatify.data.AlbumCredit
+import com.atif.spatify.data.Song
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+internal class SpatifyDataGeneratorTest {
+
+    @Test
+    fun getAlbumCredits() {
+        val albumCredits = SpatifyDataGenerator.getAlbumCredits()
+        assertThat(albumCredits.isNotEmpty()).isTrue()
+        assertThat(albumCredits[0] is AlbumCredit).isTrue()
+    }
+
+    @Test
+    fun getAlbums() {
+        val albums = SpatifyDataGenerator.getAlbums()
+        assertThat(albums.isNotEmpty()).isTrue()
+        assertThat(albums[0] is Album).isTrue()
+    }
+
+    @Test
+    fun getSongs() {
+        val songs = SpatifyDataGenerator.getSongs()
+        assertThat(songs.isNotEmpty()).isTrue()
+        assertThat(songs[0] is Song).isTrue()
+    }
+}
