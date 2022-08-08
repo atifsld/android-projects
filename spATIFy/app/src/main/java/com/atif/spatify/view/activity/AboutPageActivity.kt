f1b8cb3f844be6679b5b8aead88ed1d343abd2ff
Atif Saleed <atif.mohd.sld@gmail.com>
2022-08-08 12:57:44 +0530

    Added logo in about page

--- spATIFy/app/src/main/java/com/atif/spatify/view/activity/AboutPageActivity.kt
+++ spATIFy/app/src/main/java/com/atif/spatify/view/activity/AboutPageActivity.kt
@@ -3,6 +3,7 @@ package com.atif.spatify.view.activity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.atif.spatify.R
 import mehdi.sakout.aboutpage.AboutPage
 import mehdi.sakout.aboutpage.Element
 
@@ -13,8 +14,8 @@ class AboutPageActivity : AppCompatActivity() {
 
         val aboutPage: View = AboutPage(this)
             .setDescription("Spatify was created to be a catalogue of the world's best music. Every album listed is curated by celebrated musicologists and reviewers, and is updated on a weekly basis.")
+            .setImage(R.drawable.logosmall)
             .isRTL(false)
-            //        .setImage(R.drawable.logo)
             .addItem(Element().setTitle("Version 1.0"))
             .addGroup("Connect with us")
             .addEmail("atif.mohd.sld@gmail.com")
