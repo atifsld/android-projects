e4b0ccdeb460d55d0f7bbe519f890bfd3c7f22e1
Atif Saleed <atif.mohd.sld@gmail.com>
2022-08-24 16:07:13 +0530

    Added NavigationContainer backbone

--- react-native-projects/spATIFy/App.js
+++ react-native-projects/spATIFy/App.js
@@ -2,13 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import react from 'react';
 import SongsScreen from './assets/screens/SongsScreen';
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <View style={styles.container}>
-      <SongsScreen />
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="SongsScreen" component={SongsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
