2e89734b7b94a74f760cce1a6bc69df97b5c2ff3
Atif Saleed <atif.mohd.sld@gmail.com>
2022-08-24 18:37:26 +0530

    AlbumDetailScreen now shows songs in the album

--- react-native-projects/spATIFy/assets/screens/AlbumDetailScreen.js
+++ react-native-projects/spATIFy/assets/screens/AlbumDetailScreen.js
@@ -1,11 +1,36 @@
 import DUMMYDATA from "../mockdata/dummy-data";
 import { View, Text } from "react-native";
 import { StyleSheet } from "react-native";
+import { FlatList } from "react-native";
+import SongListTile from "../components/SongListTile";
 
-function AlbumDetailScreen() {
+function AlbumDetailScreen({ route }) {
+    const album = route.params.album;
+
+    function returnSong(itemData) {
+        return <SongListTile song={itemData.item}/>
+    }
+
+    function getSongsInAlbum(albumId) {
+        return DUMMYDATA.SONGS.filter((song) => song.songAlbumId === albumId)
+    }
+
     return (
-        <View>
-            <Text>Hello</Text>
+        <View style={styles.container}>
+            <Text>Album ID: {album.id} </Text>
+            <Text>Songs: </Text>
+            <FlatList
+                data={getSongsInAlbum(album.id)}
+                keyExtractor={(song) => song.id}
+                renderItem={returnSong}
+            >
+                
+            </FlatList>
+            {DUMMYDATA.SONGS
+                .map((song) => {
+                    <Text>song.songName</Text>
+                })
+            }
         </View>
     )
 }
@@ -14,7 +39,8 @@ const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
         flex: 1,
-        margin: 8
+        margin: 8,
+        padding: 8
     }
 })
 
--- react-native-projects/spATIFy/assets/screens/AlbumsScreen.js
+++ react-native-projects/spATIFy/assets/screens/AlbumsScreen.js
@@ -3,12 +3,13 @@ import AlbumGridTile from "../components/AlbumGridTile";
 import { View, FlatList } from "react-native";
 import { StyleSheet } from "react-native";
 
-
 function AlbumsScreen ({navigation}) {
 
     function returnAlbum(itemData) {
         function onPress() {
-            navigation.navigate('AlbumDetailScreen')
+            navigation.navigate('AlbumDetailScreen', {
+                album: itemData.item
+            })
         }
     
         return (
@@ -21,7 +22,7 @@ function AlbumsScreen ({navigation}) {
         <FlatList
             data={DUMMYDATA.ALBUMS}
             keyExtractor={(item) => item.id}
-            renderItem={returnAlbum.bind()}
+            renderItem={returnAlbum}
             numColumns = {2}
         />
         </View>
