234ae027079311b2ea659d772ffa456efdd3ee76
Atif Saleed <atif.mohd.sld@gmail.com>
2022-08-25 14:09:47 +0530

    Created AlbumCredit entity

---
+++ react-native-projects/spATIFy/assets/models/AlbumCredit.js
@@ -0,0 +1,16 @@
+class AlbumCredit {
+    constructor(
+        id, 
+        creditName, 
+        creditContribution, 
+        creditImageUrl, 
+        creditAlbumId) {
+            this.id = id
+            this.creditName = creditName
+            this.creditContribution = creditContribution
+            this.creditImageUrl = creditImageUrl
+            this.creditAlbumId = creditAlbumId
+    }
+}
+
+export default AlbumCredit;
