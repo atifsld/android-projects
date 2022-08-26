2698861ffac7dd69947ea4d15f0cb0ea829a1361
Atif Saleed <atif.mohd.sld@gmail.com>
2022-08-26 13:39:04 +0530

    Created dummy favorites screen

--- react-native-projects/spATIFy/App.js
+++ react-native-projects/spATIFy/App.js
@@ -1,33 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import react from 'react';
-import SongsScreen from './assets/screens/SongsScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import SongsScreen from './assets/screens/SongsScreen';
 import AlbumsScreen from './assets/screens/AlbumsScreen';
 import AlbumDetailScreen from './assets/screens/AlbumDetailScreen';
-
+import FavoritesScreen from './assets/screens/FavoritesScreen';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator () {
   return <Drawer.Navigator>
-      <Drawer.Screen 
-        name="AlbumsScreen" 
-        component={AlbumsScreen} 
-        options={{
-          title:  'Albums'
-        }}
-      />
-      <Drawer.Screen 
-          name="SongsScreen" 
-          component={SongsScreen} 
-          options={{
-            title:  'Songs'
-          }}
-      />
+    <Drawer.Screen 
+      name="AlbumsScreen" 
+      component={AlbumsScreen} 
+      options={{
+        title:  'Albums',
+        drawerIcon:({color, size}) => (
+          <Ionicons name="albums" color={color} size={size} />
+        )
+      }}
+    />
+    <Drawer.Screen 
+      name="SongsScreen" 
+      component={SongsScreen} 
+      options={{
+        title:  'Songs',
+        drawerIcon:({color, size}) => (
+          <Ionicons name="list" color={color} size={size} />
+        )
+      }}
+    />
+    <Drawer.Screen 
+      name="FavoritesScreen" 
+      component={FavoritesScreen} 
+      options={{
+        title:  'Favorites',
+        drawerIcon:({color, size}) => (
+          <MaterialIcons name="favorite" color={color} size={size} />
+        )
+      }}
+    />
   </Drawer.Navigator>
 }
 
---
+++ react-native-projects/spATIFy/assets/screens/FavoritesScreen.js
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native"
+import { StyleSheet } from "react-native-web"
+
+function FavoritesScreen () {
+    return (
+        <View style={styles.favoritesScreen}>
+            <Text>Hello, your favorites will be displayed here in the future</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    favoritesScreen: {
+
+    }
+})
+
+export default FavoritesScreen
