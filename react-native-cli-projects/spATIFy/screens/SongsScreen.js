de05a4ef4e1a519724d12dd6c0832fa208d7b093
Atif Saleed <atif.mohd.sld@gmail.com>
2022-09-02 14:20:23 +0530

    Revert "Search functionality"
    This reverts commit 8b61d42d26526e63eb4ffb6969b0b1089f3fcbfd.

--- react-native-cli-projects/spATIFy/screens/SongsScreen.js
+++ react-native-cli-projects/spATIFy/screens/SongsScreen.js
@@ -1,49 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import DUMMYDATA from "../mockdata/dummy-data";
 import SongListTile from "../components/SongListTile";
-import { View, FlatList, TextInput } from "react-native";
+import { View, FlatList } from "react-native";
 import { StyleSheet } from "react-native";
 
 function returnSong(itemData) {
     return <SongListTile song={itemData.item}/>
 }
 
-function SongsScreen ({navigation}) {
-    const [filteredData, setFilteredData] = useState([])
-    const [masterData, setMasterData] = useState([])
-    const [search, setSearch] = useState('')
-
-    useEffect(() => {
-        fetchData()
-        return () => {}
-    }, [])
-    
-    const fetchData = () => {
-        setFilteredData(DUMMYDATA.SONGS)
-        setMasterData(DUMMYDATA.SONGS)
-    }
-
-    const searchFilter = (text) => {
-        if(text) {
-            const newData = masterData.filter((song) => {
-                const songData = song.name? song.name.toUpperCase():''.toUpperCase()
-                const textData = text.toUpperCase
-                return itemData.indexOf(textData)>-1
-            })
-            setFilteredData(newData)
-            setSearch(text)
-        } else {
-            setFilteredData(masterData)
-            setSearch(text)
-        }
-    }
+function SongsScreen () {
     return <View>
-        <TextInput
-            style = {styles.textInputStyle}
-            value = {search}
-            placeholder = "Search"
-            underlineColorAndroid="transparent"
-            onChangeText = {(text)=>searchFilter(text)}/>
         <FlatList
             data={DUMMYDATA.SONGS}
             keyExtractor={(item) => item.id}
@@ -53,13 +19,6 @@ function SongsScreen ({navigation}) {
 }
 
 const styles = StyleSheet.create({
-    textInputStyle: {
-        height: 50,
-        borderWidth: 1,
-        margin: 5,
-        borderColor: '#009688',
-        backgroundColor: 'white'
-    },
     songsScreen: {
         marginTop: 16,
         padding: 16,
