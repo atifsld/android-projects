6ea81f4f0eade927214df2a406bd28bbf0ea68c5
Atif Saleed <atif.mohd.sld@gmail.com>
2022-09-19 10:43:26 +0530

    Duplicated import from react-native

--- react-native-cli-projects/spATIFy/components/AlbumCreditTile.js
+++ react-native-cli-projects/spATIFy/components/AlbumCreditTile.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
-import { StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 
 function AlbumCreditTile({albumCredit}) {
     return (
