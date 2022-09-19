95ff2b3d0003e8662ffbf34d53e9a990a35120a9
Atif Saleed <atif.mohd.sld@gmail.com>
2022-09-19 10:42:18 +0530

    Removed unused Pressable import

--- react-native-cli-projects/spATIFy/components/AlbumCreditTile.js
+++ react-native-cli-projects/spATIFy/components/AlbumCreditTile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image, Text, Pressable } from "react-native";
+import { View, Image, Text } from "react-native";
 import { StyleSheet } from "react-native";
 
 function AlbumCreditTile({albumCredit}) {
