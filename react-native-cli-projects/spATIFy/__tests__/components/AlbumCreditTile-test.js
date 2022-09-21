76b480cc683b6e42b73d0381f79e586e6128e5d9
Atif Saleed <atif.mohd.sld@gmail.com>
2022-09-21 16:24:46 +0530

    Minor refactoring in albumCredit test

--- react-native-cli-projects/spATIFy/__tests__/components/AlbumCreditTile-test.js
+++ react-native-cli-projects/spATIFy/__tests__/components/AlbumCreditTile-test.js
@@ -11,7 +11,29 @@ const sampleAlbumCredit = {
     "creditImageUrl": "https://i.discogs.com/PkmqjHCGbJZSl6SEgXoi6W1yRGhdEbf9K9Gnugw64hE/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTUwNDY5/OC0xMzE4NzYxOTM0/LmpwZWc.jpeg"
 }
 
+function findCreditDetail (tree, element, expectedValue) {
+    if(element === 'creditName') {
+        const creditName = tree.children[1].children[0].children[0]
+        return (creditName === expectedValue) 
+    } else if (element === 'creditContribution') {
+        const creditContribution = tree.children[1].children[1].children[0]
+        return (creditContribution === expectedValue)
+    } else {
+        return false;
+    }
+}
+
 test('test album credit tile', () => {
     const tree = renderer.create(<AlbumCreditTile albumCredit={sampleAlbumCredit} />).toJSON();
     expect(tree).toMatchSnapshot();
+})
+
+test('test album credit has the correct name', () => {
+    const tree = renderer.create(<AlbumCreditTile albumCredit={sampleAlbumCredit} />).toJSON();
+    expect(findCreditDetail(tree, 'creditName', 'Acy R. Lehman')).toBeTruthy()
+})
+
+test('test album credit has the correct contribution', () => {
+    const tree = renderer.create(<AlbumCreditTile albumCredit={sampleAlbumCredit} />).toJSON();
+    expect(findCreditDetail(tree, 'creditContribution', 'Design [Cover]')).toBeTruthy()
 })
